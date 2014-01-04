2ac0910117326d99f4338e9e9b4bffd4c395c944
Chong-Yee Khoo <mail@cykhoo.com>
2014-01-04 11:02:48 +0800

    compose methods to deal with intro and verse by day

--- lib/twelve_days.rb
+++ lib/twelve_days.rb
@@ -2,9 +2,26 @@ class TwelveDays
 
   def sing_verse(day_number)
     if day_number == 1
-      "On the first day of Christmas, my true love gave to me a partridge in a pear tree."
+      intro(1) + verse(1) + "."
     elsif day_number == 2
-      "On the second day of Christmas, my true love gave to me two turtle doves and a partridge in a pear tree."
+      intro(2) + "two turtle doves and " + verse(1) + "."
     end
+  end
+
+  def verse(day_number)
+    if day_number == 1
+      "a partridge in a pear tree"
+    end
+  end
+
+  def intro(day_number)
+
+    if day_number == 1
+      day_number_in_words = "first"
+    elsif day_number == 2
+      day_number_in_words = "second"
+    end
+
+    "On the " + day_number_in_words + " day of Christmas, my true love gave to me "
   end
 end
